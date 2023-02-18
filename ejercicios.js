81033f91de2643cc36e79447542f491628d43e5d
Emanuel Saucedo <emanuelsaucedo52@gmail.com>
2023-02-17 21:17:40 -0600

    segunda parte de ejercicios JS

--- ejercicios.js
+++ ejercicios.js
@@ -1,12 +1,10 @@
-// Escribir una funcion que regrese la cantidad de valores true que hay en un array
-/* 
+//////////////// Problema 1/////////////////////////////
+
+/* Escribir una funcion que regrese la cantidad de valores true que hay en un array
 countTrue([true, false, false, true, false]) ➞ 2
-
 countTrue([false, false, false, false]) ➞ 0
-
 countTrue([]) ➞ 0
- */
-// Regresar 0 si el array es vacio
+Regresar 0 si el array es vacio */
 const arr2 =  [true,false,true];
 function filterTrue(arr2) {
     return arr2 != false;
@@ -14,28 +12,24 @@ function filterTrue(arr2) {
 const countTrue = arr2 =>{
     return arr2.filter(filterTrue).length;
 } 
-// console.log(countTrue(arr2));
+console.log(countTrue(arr2));
 
+////////////////Fin - Problema 1/////////////////////////////
 
-
-
-// Escribir una funcion para encontrar el mayor comun divisor de 2 numeros positivos enteros
-// Usar recursion
-let i = 2;
-const mayorComunDivisor = (num1, num2,i) =>{
-    
-    if( num1%i == 0 &&  num2%i == 0){
-
-        return i;
-    }else{
-        i++
-        mayorComunDivisor(num1,num2,i);
+//////////////// Problema 2/////////////////////////////
+function mcd(a, b) {
+    // Si b es 0, entonces a es el MCD y se devuelve a
+    if (b == 0) {
+        return a;
     }
+    // De lo contrario, llamamos a la función de manera recursiva pasando b y el resto de a/b como parámetros
+    return mcd(b, a % b);
 }
-// console.log(i);
-console.log(mayorComunDivisor(4,8,i));
+console.log(mcd(20,5));
 
+////////////////Fin - Problema 2/////////////////////////////
 
+//////////////// Problema 3/////////////////////////////
 
 // Encontrar los primeros n elementos de la serie fibonacci
 // Usar recursion
@@ -51,12 +45,14 @@ const fibNumbers = (num,exit,arr) =>{
         arr.push(fib(exit));
         exit++;
         fibNumbers(num,exit,arr);
-    }else{
-    console.log(arr); 
     }
+return arr;
 }
-//  fibNumbers(5,exit,arr);
+ console.log(fibNumbers(6,exit,arr));
 
+////////////////Fin - Problema 3/////////////////////////////
+
+ //////////////// Problema 4/////////////////////////////
 // // reto 3
  var hour = new Date().getHours();
  var day = new Date().getDay();
@@ -79,4 +75,91 @@ const isCorrect = (day,hour) =>{
         return msg;
     }
 }
+console.log(isCorrect(hour,day));
+////////////////Fin - Problema 4/////////////////////////////
+
+
+//////////////// Problema 5/////////////////////////////
+// Encontrar el numero n de una figura piramidal triangular (tetrahedron)
+// (nivel) -> cantidad de numeritos
+let triangleAdd = [];
+let start = 1;
+let tetrahedralAdd = [];
+const intento2 = (num,triangleAdd,tetrahedralAdd,start)=>{
+    if(start<=num){
+        triangleAdd.push(start);
+        start++;
+        let prueba = triangleAdd.reduce((a, b) => a + b, 0);
+        tetrahedralAdd.push(prueba);
+        intento2(num,triangleAdd,tetrahedralAdd,start)
+    }else{
+        let prueba = tetrahedralAdd.reduce((a, b) => a + b, 0);
+        console.log(prueba);
+        
+    }
+}
+intento2(7,triangleAdd,tetrahedralAdd,start);
+
+
+const tetrahedralNumber = num =>{
+    let res = (num * (num+1)*(num+2))/6;
+    return res;
+}
+console.log(tetrahedralNumber(7));
+
+////////////////Fin- Problema 5/////////////////////////////
+
+//////////////// Problema 6/////////////////////////////
+// Crear una funcion que tome el nombre de un pais y su area
+// (nombre, a)
+// Debe regresar la proporcion del area del pais en acorde a el area de tierra en el planeta
+//('Russia', 17098242) -> Russia is 11.48%
+// ("USA", 9372610) -> "USA is 6.29%
+
+const landSpace = (country, size) =>{
+    const world = 148940000;
+    return `${country} is ${ (size * 100)/world}%`;
+}
+console.log(landSpace('Russia', 17098242));
+
+////////////////Fin -  Problema 6/////////////////////////////
+
+
+//////////////// Problema 7/////////////////////////////
+// Escribir una funcion que retorne 0 si el input es 1 y 1 si el input es 0
+// No se pueden utilizar condicionales, ternarios, negaciones ni operatores bit
+const ceroOne = num =>{
+    let arr = [1,0];
+    return arr[num];
+}
+console.log(ceroOne(0));
+////////////////Fin - Problema 7/////////////////////////////
+
+
+//////////////// Problema 8/////////////////////////////
+// Messenger bzzz
+// (0 ) n
+// Si no hay nadie -> "No hay nadie en linea"
+// Si hay 1 persona -> "user1 esta en linea"
+// Si hay 2 personas -> "user1 y user2 estan en linea"
+// Si hay n>2 personas, "user1 y n-1 mas estan en linea"
+
+const msgBzzz = num =>{
+   
+        let arr = ["No hay nadie en linea","user1 esta en linea","user1 y user2 estan en linea"];
+    if(arr.length<=num){
+        console.log(`user1 y ${num-1} mas estan conectados`);
+    }else{
+        if( num < 0 ){
+            console.log('No hay nadie en linea');
+        }else{
+            console.log(arr[num]);
+        }
+    }
+    
+}
+msgBzzz(1001);
+////////////////Fin - Problema 8/////////////////////////////
+
+
 
